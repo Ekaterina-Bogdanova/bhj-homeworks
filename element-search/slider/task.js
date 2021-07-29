ab83634b69049c9c6818b9812ef795dadfb10fc6
Ekaterina Bogdanova <bekaterina688@gmail.com>
2021-07-29 13:57:38 +0300

    Improved validation

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,33 +4,27 @@ let sliders = Array.from(document.getElementsByClassName('slider__item'));
 
 let index = 0;
 
-function Scroll(index) {
-    sliders.forEach((item) => {
-        if (item.className.includes('slider__item slider__item_active')) {
-            item.className = 'slider__item';
-        }
-    });
+function changeImage(index) {
+    sliders.find((item) => {return item.className.includes('slider__item slider__item_active')}).className = 'slider__item';
     sliders[index].className = 'slider__item slider__item_active';
 }
 
 clickPrev[0].onclick = function() {
     if (index === 0) {
         index = sliders.length -1;
-        Scroll(index);
     } else if (index !== 0) {
         index--;
-        Scroll(index);
-    }            
+    }
+    changeImage(index);            
 }
 
 clickNext[0].onclick = function() {
     if (index < sliders.length -1) {
         index++;
-        Scroll(index);
     } else if (index === sliders.length -1) {
         index = 0;
-        Scroll(index);
-    }    
+    } 
+    changeImage(index);   
 }
 
 
