f66984ca69fc18f53b3a42cdfccbaf4228601621
Ekaterina Bogdanova <bekaterina688@gmail.com>
2021-07-29 13:53:41 +0300

    Universal check and close menu on repeat

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,10 +2,15 @@ const menuMain = Array.from(document.getElementsByClassName('menu menu_main'));
 
 Array.from(menuMain[0].querySelectorAll('.menu__link')).forEach((links) => {
     links.onclick = function() {
-        if (this.textContent != 'Главная' || this.textContent != 'Контакты') {
-            let menuActive = Array.from(document.getElementsByClassName('menu menu_sub menu_active'));
+         
+        if (!this.closest('li').querySelector('ul').classList.contains('menu menu_sub')) {
+            if (this.closest('li').querySelector('ul').className.includes('menu menu_sub menu_active')) {
+                this.closest('li').querySelector('ul').className = ('menu menu_sub')                
+            } else {
+                let menuActive = Array.from(document.getElementsByClassName('menu menu_sub menu_active'));
             menuActive.forEach((item) => {item.className = 'menu menu_sub'});
             this.closest('li').querySelector('ul').className = 'menu menu_sub menu_active';
+            }     
             return false;
         }
     }      
